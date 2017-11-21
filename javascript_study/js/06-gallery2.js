85f883021833b983360b4daa6535ef6139cc9969
fattoliu <724684054@qq.com>
2017-11-21 10:04:46 +0800

    重新整理相册的结构，去除底部 img 和 p 标签，利用动态创建的方法做，彻底分离结构、样式和行为

--- javascript_study/js/06-gallery2.js
+++ javascript_study/js/06-gallery2.js
@@ -1,5 +1,7 @@
 addLoadEvent(prepareGallery);
+addLoadEvent(preparePlaceholder);
 
+/*显示图片*/
 function showPic(whichpic) {
     if (!document.getElementById("placeholder")) return false;
     var source = whichpic.getAttribute("href");
@@ -17,6 +19,7 @@ function showPic(whichpic) {
     return true;
 }
 
+/*相册列表点击事件分配*/
 function prepareGallery() {
     if (!document.getElementsByTagName) return false;
     if (!document.getElementById) return false;
@@ -30,6 +33,62 @@ function prepareGallery() {
     }
 }
 
+/*创建图片和描述*/
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+
+    var placeholderImage = document.createElement("img");
+    placeholderImage.setAttribute("id","placeholder");
+    placeholderImage.setAttribute("src", "../html_css_study/images/banner_one.jpg");
+    placeholderImage.setAttribute("alt", "my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id", "description");
+    var desctext = document.createTextNode("Choose an image");
+    description.appendChild(desctext);
+    // 传统方法
+    // document.getElementsByTagName("body")[0].appendChild(placeholderImage);
+    // document.getElementsByTagName("body")[0].appendChild(description);
+
+    // insertBefore方法
+    /*调用改方法的必须要告诉它三件事：
+    *   1.新元素：你想插入的元素（newElement）
+    *   2.目标元素：你想把这个元素插入到哪个元素（targetElement）之前
+    *   3.父元素：目标元素的父元素（parentElement）
+    *   parentElement.insertBefore（newElement，targetElement）*/
+    // var imagegallery = document.getElementById("imagegallery");
+    // imagegallery.parentNode.insertBefore(placeholderImage,imagegallery);
+    // imagegallery.parentNode.insertBefore(description,imagegallery);
+
+    // 自己编写 insertAfter 函数（因为系统没提供该方法）
+    var imagegallery = document.getElementById("imagegallery");
+    insertAfter(placeholderImage, imagegallery);
+    insertAfter(description, placeholderImage);
+
+
+}
+
+// *********************************************  以下为通用方法 *********************************************
+
+
+/* 自己编写 insertAfter 函数（因为系统没提供该方法）
+*/
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    // 如果目标元素是父元素中最后一个，那newElement直接以子元素形式添加到父元素上
+    if (parent.lastChild === targetElement) {
+        parent.appendChild(newElement);
+    } else {
+        // 如果不是最后一个，就把新元素插入到目标元素和目标元素的下一个兄弟元素之间，目标元素的下一个兄弟元素即目标元素的 nextSibling 属性
+        // 用insertBefore 把新元素插入到目标元素的下一个兄弟元素之前：
+        alert(targetElement.nextSibling.nodeName);
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
+/*加载界面是添加事件函数*/
 function addLoadEvent(func) {
     var oldonload = window.onload;
     if (typeof window.onload !== 'function') {
