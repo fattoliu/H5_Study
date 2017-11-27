587c8ef1d71fa06d382bf72eb98c7e789a315c8d
fattoliu <724684054@qq.com>
2017-11-27 16:06:14 +0800

    完成模板页、Home页、About页的编写

---
+++ Jay Skript and the Domsters/js/base.js
@@ -0,0 +1,119 @@
+// *********************************************  以下为通用方法 *********************************************
+
+/*加载界面是添加事件函数*/
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload !== 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+
+/* 自己编写 insertAfter 函数（因为系统没提供该方法）
+*/
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    // 如果目标元素是父元素中最后一个，那newElement直接以子元素形式添加到父元素上
+    if (parent.lastChild === targetElement) {
+        parent.appendChild(newElement);
+    } else {
+        // 如果不是最后一个，就把新元素插入到目标元素和目标元素的下一个兄弟元素之间，目标元素的下一个兄弟元素即目标元素的 nextSibling 属性
+        // 用insertBefore 把新元素插入到目标元素的下一个兄弟元素之前：
+        // alert(targetElement.nextSibling.nodeName);
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
+/*获取下一个元素节点*/
+function getNextElement(node) {
+    // 如果是元素节点，直接返回
+    if (node.nodeType == 1) {
+        return node;
+    }
+    // 如果有下一个节点，就递归
+    if (node.nextSibling) {
+        return getNextElement(node.nextSibling);
+    }
+    return null;
+}
+
+/*为元素添加 class 属性，追加属性，而非替换*/
+function addClass(element, value) {
+    if(!element.className) {
+        element.className = value;
+    } else {
+        element.className += " " + value;
+    }
+}
+
+function moveElement(elementID, final_x, final_y, interval) {
+    if (!document.getElementById) return false;
+    if (!document.getElementById(elementID)) return false;
+    var elem = document.getElementById(elementID);
+    //判断 movement 是否存在，存在的话，先调用 clearTimeout，这样就不会存在动画错乱的问题了
+    if (elem.movement) {
+        clearTimeout(elem.movement);
+    }
+
+    //如果 elem 的 left 和 top 属性未设置，会报错，所以先设置默认值
+    if (!elem.style.left) {
+        elem.style.left = "0px";
+    }
+    if (!elem.style.top) {
+        elem.style.top = "0px";
+    }
+
+    var xpos = parseInt(elem.style.left);
+    var ypos = parseInt(elem.style.top);
+    if (xpos == final_x && ypos == final_y) {
+        return true;
+    }
+
+    /*以下代码执行的动画效果将是匀速的，因为每次都是+1，所以如果目标位置离当前位置很远的话，动画太慢，可以改进*/
+    // if (xpos < final_x) {
+    //     xpos++;
+    // }
+    // if (xpos > final_x) {
+    //     xpos--;
+    // }
+    // if (ypos < final_y) {
+    //     ypos++;
+    // }
+    // if (ypos > final_y) {
+    //     ypos--;
+    // }
+
+    // 改进版
+    var dist = 0;
+    if (xpos < final_x) {
+        // 把元素朝他的目的地移动十分之一的距离
+        dist =  Math.ceil((final_x-xpos)/20);
+        xpos = xpos+dist;
+    }
+    if (xpos > final_x) {
+        dist =  Math.ceil((xpos-final_x)/20);
+        xpos = xpos-dist;
+    }
+
+    if (ypos < final_y) {
+        // 把元素朝他的目的地移动十分之一的距离
+        dist =  Math.ceil((final_y-ypos)/20);
+        ypos = ypos+dist;
+    }
+    if (xpos > final_x) {
+        dist =  Math.ceil((ypos-final_y)/20);
+        ypos = ypos-dist;
+    }
+
+    elem.style.left = xpos + "px";
+    elem.style.top = ypos + "px";
+    // moveElement("elementID',final_x,final_y,interval)
+    var repeat = "moveElement('" + elementID + "'," + final_x + "," + final_y + "," + interval + ")";
+    // 需要给这个 elem 定义一个属性 movement，这样在鼠标快速切换产生移动元素的动画是，就可以先判断 movement 是否存在
+    // 存在的话，先调用 clearTimeout，这样就不会存在动画错乱的问题了
+    elem.movement = setTimeout(repeat, interval);
+}
---
+++ Jay Skript and the Domsters/js/global.js
@@ -0,0 +1,102 @@
+function highlightPage() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    var headers = document.getElementsByTagName("header");
+    if (headers.length == 0) return false;
+    var navs = headers[0].getElementsByTagName("nav");
+    if (navs.length == 0) return false;
+    var links = navs[0].getElementsByTagName("a");
+    var linkurl;
+    for (var i = 0; i < links.length; i++) {
+        linkurl = links[i].getAttribute("href");
+        if (window.location.href.indexOf(linkurl) != -1) {
+            links[i].className = "here";
+            var linktext = links[i].lastChild.nodeValue.toLowerCase();
+            document.body.setAttribute("id", linktext);
+        }
+    }
+}
+
+function prepareSlideshow() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("intro")) return false;
+
+    var intro = document.getElementById("intro");
+    var slideshow = document.createElement("div");
+    slideshow.setAttribute("id", "slideshow");
+
+    var frame = document.createElement("img");
+    frame.setAttribute("src", "images/frame.gif");
+    frame.setAttribute("alt", "");
+    frame.setAttribute("id", "frame");
+    slideshow.appendChild(frame);
+    var preview = document.createElement("img");
+    preview.setAttribute("src", "images/slideshow.gif");
+    preview.setAttribute("alt", "a glimpse of what awaits you");
+    preview.setAttribute("id", "preview");
+    slideshow.appendChild(preview);
+    insertAfter(slideshow, intro);
+
+    var links = document.getElementsByTagName("a");
+    var destination;
+    for(var i = 0; i < links.length; i++) {
+        links[i].onmouseover = function () {
+            destination = this.getAttribute("href");
+            if (destination.indexOf("index.html") != -1) {
+                moveElement("preview",0,0,5);
+            }
+            if (destination.indexOf("about.html") != -1) {
+                moveElement("preview",-150,0,5);
+            }
+            if (destination.indexOf("photos.html") != -1) {
+                moveElement("preview",-300,0,5);
+            }
+            if (destination.indexOf("live.html") != -1) {
+                moveElement("preview",-450,0,5);
+            }
+            if (destination.indexOf("contact.html") != -1) {
+                moveElement("preview",-600,0,5);
+            }
+        }
+    }
+}
+
+function showSelection(id) {
+    var sections = document.getElementsByTagName("section");
+    for(var i=0; i < sections.length; i++) {
+        if (sections[i].getAttribute("id") != id) {
+            sections[i].style.display = "none";
+        } else {
+            sections[i].style.display = "block";
+        }
+    }
+}
+
+function prepareInternalnav() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    var articles = document.getElementsByTagName("article");
+    if (articles.length == 0) return false;
+    var navs = articles[0].getElementsByTagName("nav");
+    if (navs.length == 0) return false;
+    var nav = navs[0];
+    var links = nav.getElementsByTagName("a");
+    for (var i = 0; i < links.length;i++) {
+        var sectionId = links[i].getAttribute("href").split("#")[1];
+        if (!document.getElementById(sectionId)) continue;
+        document.getElementById(sectionId).style.display = "none";
+        //由于 sectionId 是个局部变量，存在作用域的问题，只有在该函数执行期间存在，等到时间处理函数执行的时候就不存在了，所以为每个 link
+        // 创建一个自定义的属性
+        links[i].destination = sectionId;
+        links[i].onclick = function () {
+            showSelection(this.destination);
+            return false;
+        }
+    }
+}
+
+addLoadEvent(highlightPage);
+addLoadEvent(prepareSlideshow);
+addLoadEvent(showSelection);
+addLoadEvent(prepareInternalnav);
